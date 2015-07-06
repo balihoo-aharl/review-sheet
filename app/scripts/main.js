666603e245f609e58aea5649a85185533234c954
Andy Harl <aharl@balihoo.com>
2015-07-06 11:34:09 -0600

    Updated checklist with advanced options

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+/*global $*/
+
+$(function() {
+
+  $('.review-type button').click(function(event) {
+    event.preventDefault();
+    $(this).addClass('active');
+    $(this).siblings('button').removeClass('active');
+
+    if ($(this).hasClass('show-basic')) {
+      $('label.adv').fadeOut('500');
+    } else if ($(this).hasClass('show-adv')) {
+      $('label.adv').fadeIn('500');
+    }
+  });
+
+});
